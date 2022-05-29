abb95a8908839fd0e3da080ed028d7ae6ce74917
pace11 <ryanjoker87@gmail.com>
2022-05-29 11:17:45 +0900

    feat: update

--- src/pages/contact-us/index.js
+++ src/pages/contact-us/index.js
@@ -1,9 +1,61 @@
-import React from "react"
+import React, { useEffect } from 'react'
 
 export default function ContactUs() {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+  
   return (
-    <div>
-      <p>Contact Us</p>
-    </div>
+    <React.Fragment>
+      <div class="container-xxl py-5">
+        <div class="container">
+          <div class="row wow fadeInUp">
+            <div class="col-lg-6 col-md-6">
+              <img
+                src={require('../../assets/image/surat.svg').default}
+                alt="surat"
+              />
+            </div>
+            <div class="col-lg-6 col-md-6">
+              <div class="row">
+                <div class="col-lg-6 col-md-6">
+                  <input
+                    class="form-control"
+                    placeholder="Nama depan"
+                    type="text"
+                  />
+                </div>
+                <div class="col-lg-6 col-md-6">
+                  <input
+                    class="form-control"
+                    placeholder="Nama belakang"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div class="row mt-4">
+                <div class="col-lg-12 col-md-12">
+                  <input
+                    class="form-control"
+                    placeholder="Email"
+                    type="email"
+                  />
+                </div>
+              </div>
+              <div class="row mt-4">
+                <div class="col-lg-12 col-md-12">
+                  <textarea class="form-control" placeholder="Pesan" />
+                </div>
+              </div>
+              <div class="row mt-4">
+                <div class="col-lg-12 col-md-12 d-flex justify-content-end">
+                  <button class="btn btn-primary">Kirim</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
   )
 }
